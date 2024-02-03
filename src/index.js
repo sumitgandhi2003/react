ce58a07f386f575f5b55c120b2e06382ff65ecb1
Sumit Gandhi <sumitgandhi932@gmail.com>
2024-02-03 18:43:14 +0530

    add

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const heading = <h1 className="hello">hello</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(heading);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
